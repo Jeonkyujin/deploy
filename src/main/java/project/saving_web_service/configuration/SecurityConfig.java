57439616a0e47a6513647ef0902b3d74a3606eb2
Jeonkyujin <954qew@naver.com>
2024-11-08 22:05:46 +0900

    ㅇㅇ

--- src/main/java/project/saving_web_service/configuration/SecurityConfig.java
+++ src/main/java/project/saving_web_service/configuration/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(AbstractHttpConfigurer::disable)
+			.csrf(csrf -> csrf.disable())
 			.authorizeRequests(authorize -> authorize
 				.anyRequest().authenticated()
 			);
