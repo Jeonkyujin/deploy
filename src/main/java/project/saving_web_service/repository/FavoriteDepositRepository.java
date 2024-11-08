3131911b6bb3eef9d1b11939dc55c7d2ca067bef
Jeonkyujin <954qew@naver.com>
2024-11-08 21:42:41 +0900

    ㅇㅇ

--- src/main/java/project/saving_web_service/repository/FavoriteDepositRepository.java
+++ src/main/java/project/saving_web_service/repository/FavoriteDepositRepository.java
@@ -4,13 +4,14 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import project.saving_web_service.domain.FavoriteDeposit;
 import project.saving_web_service.domain.FavoriteInstall;
 
-@Repository
+@EnableJpaRepositories
 public interface FavoriteDepositRepository extends JpaRepository<FavoriteDeposit, Long> {
 
 	@Query("SELECT f FROM FavoriteDeposit f WHERE f.member.id = :memberId AND f.deposit.예금id = :depositId")
