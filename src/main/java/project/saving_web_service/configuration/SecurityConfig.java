2186026ae9b192eff638131da9748d361f29ce70
Jeonkyujin <954qew@naver.com>
2024-11-12 14:42:46 +0900

    commit

--- src/main/java/project/saving_web_service/configuration/SecurityConfig.java
+++ src/main/java/project/saving_web_service/configuration/SecurityConfig.java
@@ -1,24 +1,29 @@
-
-/*package project.saving_web_service.configuration;
+package project.saving_web_service.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig{
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable())
-			.authorizeRequests(authorize -> authorize
-				.anyRequest().authenticated()
-			);
-
+			.csrf(csrf -> csrf.disable())// CSRF 보호 비활성화
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/login").permitAll()// 로그인 URL 허용
+			.anyRequest().authenticated()
+			);// 그 외의 요청은 인증 필요
 		return http.build();
 	}
-
-}*/
+}
