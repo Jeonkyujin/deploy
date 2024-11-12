a31fc034599323d0437184800c7530c93ca8ba01
Jeonkyujin <954qew@naver.com>
2024-11-12 19:39:07 +0900

    commit

--- src/main/java/project/saving_web_service/configuration/SecurityConfig.java
+++ src/main/java/project/saving_web_service/configuration/SecurityConfig.java
@@ -6,20 +6,24 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import lombok.AllArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
-@AllArgsConstructor
+
 public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
-			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/**").permitAll() // 모든 경로에 접근 권한 허용
+			// 403, "http.csrf.disable()" deprecated
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+				// HTTP 요청에 대한 권한 설정, 모든 경로로 지정("/**")
+				// permitAll() : 해당 요청에 대한 모든 사용자에게 접근 권한 부여
+				.requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
 			);
 		return http.build();
 	}
