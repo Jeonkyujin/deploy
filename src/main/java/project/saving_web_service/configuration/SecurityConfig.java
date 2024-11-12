107872b47390c1666de4c762c407ab00717b312d
Jeonkyujin <954qew@naver.com>
2024-11-12 16:51:13 +0900

    commit

--- src/main/java/project/saving_web_service/configuration/SecurityConfig.java
+++ src/main/java/project/saving_web_service/configuration/SecurityConfig.java
@@ -10,16 +10,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
 
-@Configuration
+
 @EnableWebSecurity
-@EnableMethodSecurity
-@RequiredArgsConstructor
 public class SecurityConfig{
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-
+			.csrf(csrf -> csrf.disable())
 			.authorizeHttpRequests(auth -> auth
 				.anyRequest().permitAll()
 			);
