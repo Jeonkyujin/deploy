0ca4f02b1ca10008fa20279d5d6d1eb2f625c054
Jeonkyujin <954qew@naver.com>
2024-11-12 15:59:14 +0900

    commit

--- src/main/java/project/saving_web_service/configuration/SecurityConfig.java
+++ src/main/java/project/saving_web_service/configuration/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig{
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable())// CSRF 보호 비활성화
+
 			.authorizeHttpRequests(auth -> auth
 				.anyRequest().permitAll()
 			);
