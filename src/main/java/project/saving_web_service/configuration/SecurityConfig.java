3c027af72121d7431db00efa0e07827562985dcb
Jeonkyujin <954qew@naver.com>
2024-11-12 17:21:24 +0900

    commit

--- src/main/java/project/saving_web_service/configuration/SecurityConfig.java
+++ src/main/java/project/saving_web_service/configuration/SecurityConfig.java
@@ -1,25 +1,24 @@
 package project.saving_web_service.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 
-
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+@AllArgsConstructor
+public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable())
+			.csrf(csrf -> csrf.disable()) // CSRF 비활성화
 			.authorizeHttpRequests(auth -> auth
-				.anyRequest().permitAll()
+				.requestMatchers("/**").permitAll() // 모든 경로에 접근 권한 허용
 			);
 		return http.build();
 	}
