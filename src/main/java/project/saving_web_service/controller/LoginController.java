d72b544f6a2acc1d8cb5d14b2132c5254fca2260
Jeonkyujin <954qew@naver.com>
2024-11-23 21:24:25 +0900

    필요 없는 파일 제거2

--- src/main/java/project/saving_web_service/controller/LoginController.java
+++ src/main/java/project/saving_web_service/controller/LoginController.java
@@ -5,17 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import project.saving_web_service.domain.Member;
 import project.saving_web_service.service.MemberService;
-import project.saving_web_service.service.NewsService;  // 뉴스 서비스 추가
 import project.saving_web_service.service.RedisService;
 
-
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -26,7 +20,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class LoginController {
 
     private final MemberService memberService;
-    private final NewsService newsService;
     private final RedisService redisService;
 
     @GetMapping("/login")
@@ -50,14 +43,13 @@ public class LoginController {
         model.addAttribute("string", strings);
         model.addAttribute("member", member);
         model.addAttribute("login_id", login_id);
-        List<Map<String, String>> newsList = newsService.getLatestNews();
-        model.addAttribute("newsList", newsList);
+
+
         if (httpSession.getAttribute("firstLogin") == null) {
             httpSession.setAttribute("firstLogin", true);
 
         } else {
             if (!currentTopProduct.equals(previousTopProduct)) {
-                System.out.println("---------------");
                 model.addAttribute("showAlert", true);
                 String alertMessage = "1위 상품이 변경되었습니다.<br>" + currentTopProduct;
                 model.addAttribute("alertMessage", alertMessage);
